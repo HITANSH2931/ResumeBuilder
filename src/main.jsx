f6fa39b5aedea94ab7a14ef85f14cc673ce373fa
HITANSH2931 <hitanshjoshi01@gmail.com>
2025-11-25 08:33:02 +0530

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import store from './Redux/store.js'
 import 'react-toastify/dist/ReactToastify.css'
 import {ToastContainer} from 'react-toastify'
 
